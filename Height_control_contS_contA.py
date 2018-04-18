9479a564bc068be79ceb055cd2ee09941b2bc83d
panin <pienrojp@student.ethz.ch>
2018-04-18 10:09:54 +0200

    first ver

--- Height_control_contS_contA.py
+++ Height_control_contS_contA.py
@@ -11,6 +11,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from gym import wrappers
 from datetime import datetime
+
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.kernel_approximation import RBFSampler
+
 #from q_learning import plot_running_avg, FeatureTransformer, plot_cost_to_go
 
 
@@ -46,10 +51,10 @@ class FeatureTransformer:
     # Used to converte a state to a featurizes represenation.
     # We use RBF kernels with different variances to cover different parts of the space
     featurizer = FeatureUnion([
-            ("rbf1", RBFSampler(gamma=2.0, n_components=n_components)),
-            ("rbf2", RBFSampler(gamma=1.0, n_components=n_components)),
-            ("rbf3", RBFSampler(gamma=0.5, n_components=n_components)),
-            ("rbf4", RBFSampler(gamma=0.1, n_components=n_components))
+            ("rbf1", RBFSampler(gamma=5.0, n_components=n_components)),
+            ("rbf2", RBFSampler(gamma=2.0, n_components=n_components)),
+            ("rbf3", RBFSampler(gamma=1.0, n_components=n_components)),
+            ("rbf4", RBFSampler(gamma=0.5, n_components=n_components))
             ])
     example_features = featurizer.fit_transform(scaler.transform(observation_examples))
 
@@ -65,7 +70,7 @@ class FeatureTransformer:
 
 # so you can test different architectures
 class HiddenLayer:
-  def __init__(self, M1, M2, f=tf.nn.tanh, use_bias=True, zeros=False):
+  def __init__(self, M1, M2, f=tf.nn.relu, use_bias=True, zeros=False):
     if zeros:
       W = np.zeros((M1, M2), dtype=np.float32)
     else:
@@ -88,131 +93,124 @@ class HiddenLayer:
 
 # approximates pi(a | s)
 class PolicyModel:
-  def __init__(self, D, ft, hidden_layer_sizes=[]):
-    self.ft = ft
+    def __init__(self, D, ft, hidden_layer_sizes=[]):
+        self.ft = ft
 
     ##### hidden layers #####
-    M1 = D
+        M1 = D
     
-    self.hidden_layers = []
+        self.hidden_layers = []
 
-    for M2 in hidden_layer_sizes:
-      layer = HiddenLayer(M1, M2)
-      self.hidden_layers.append(layer)
-      M1 = M2
+        for M2 in hidden_layer_sizes:
+            layer = HiddenLayer(M1, M2)
+            self.hidden_layers.append(layer)
+            M1 = M2
 
     # final layer mean
-    self.mean_layer = HiddenLayer(M1, 1, lambda x: x, use_bias=False, zeros=True)
+        self.mean_layer = HiddenLayer(M1, 1, lambda x: x, use_bias=False, zeros=True)
 
     # final layer variance
-    self.stdv_layer = HiddenLayer(M1, 1, tf.nn.softplus, use_bias=False, zeros=False)
+        self.stdv_layer = HiddenLayer(M1, 1, tf.nn.softplus, use_bias=False, zeros=False)
 
     # inputs and targets
-    self.X = tf.placeholder(tf.float32, shape=(None, D), name='X')
-    self.actions = tf.placeholder(tf.float32, shape=(None,), name='actions')
-    self.advantages = tf.placeholder(tf.float32, shape=(None,), name='advantages')
+        self.X = tf.placeholder(tf.float32, shape=(None, D), name='X')
+        self.actions = tf.placeholder(tf.float32, shape=(None,), name='actions')
+        self.advantages = tf.placeholder(tf.float32, shape=(None,), name='advantages')
 
     # get final hidden layer
-    Z = self.X
-    for layer in self.hidden_layers:
-      Z = layer.forward(Z)
+        Z = self.X
+        for layer in self.hidden_layers:
+            Z = layer.forward(Z)
 
     # calculate output and cost
-    mean = self.mean_layer.forward(Z)
-    stdv = self.stdv_layer.forward(Z) + 1e-5 # smoothing
+        mean = self.mean_layer.forward(Z)
+        stdv = self.stdv_layer.forward(Z) + 1e-5 # smoothing
 
     # make them 1-D
-    mean = tf.reshape(mean, [-1])
-    stdv = tf.reshape(stdv, [-1]) 
+        mean = tf.reshape(mean, [-1])
+        stdv = tf.reshape(stdv, [-1]) 
 
-    norm = tf.contrib.distributions.Normal(mean, stdv)
-    self.predict_op = norm.sample()
+        norm = tf.contrib.distributions.Normal(mean, stdv)
+        self.predict_op = norm.sample()
     
     #log pi(a|s)
-    log_probs = norm.log_prob(self.actions)
+        log_probs = norm.log_prob(self.actions)
     
-    cost = -tf.reduce_sum(self.advantages * log_probs + 0.1*norm.entropy())
-    self.train_op = tf.train.AdamOptimizer(1e-3).minimize(cost)
+        cost = -tf.reduce_sum(self.advantages * log_probs + 0.1*norm.entropy())
+        self.train_op = tf.train.AdamOptimizer(1e-3).minimize(cost)
 
-  def set_session(self, session):
-    self.session = session
+    def set_session(self, session):
+        self.session = session
 
-  def partial_fit(self, X, actions, advantages):
-    X = np.atleast_2d(X)
-    X = self.ft.transform(X)
+    def partial_fit(self, X, actions, advantages):
+        X = np.atleast_2d(X)
+        X = self.ft.transform(X)
     
-    actions = np.atleast_1d(actions)
-    advantages = np.atleast_1d(advantages)
-    self.session.run(
-      self.train_op,
-      feed_dict={
-        self.X: X,
-        self.actions: actions,
-        self.advantages: advantages,
-      }
-    )
+        actions = np.atleast_1d(actions)
+        advantages = np.atleast_1d(advantages)
+        self.session.run(self.train_op,feed_dict={self.X: X,self.actions: actions,self.advantages: advantages})
 
-  def predict(self, X):
-    X = np.atleast_2d(X)
-    X = self.ft.transform(X)
-    return self.session.run(self.predict_op, feed_dict={self.X: X})
+    def predict(self, X):
+        X = np.atleast_2d(X)
+        X = self.ft.transform(X)
+        return self.session.run(self.predict_op, feed_dict={self.X: X})
 
-  def sample_action(self, X):
-    p = self.predict(X)[0]
-    return p
+    def sample_action(self, X):
+        p = self.predict(X)[0]
+        return p
 
 
 # approximates V(s)
 class ValueModel:
-  def __init__(self, D, ft, hidden_layer_sizes=[]):
-    self.ft = ft
-    self.costs = []
+    def __init__(self, D, ft, hidden_layer_sizes=[]):
+        self.ft = ft
+        self.costs = []
 
     # create the graph
-    self.layers = []
-    M1 = D
-    for M2 in hidden_layer_sizes:
-      layer = HiddenLayer(M1, M2)
-      self.layers.append(layer)
-      M1 = M2
+        self.layers = []
+        M1 = D
+        for M2 in hidden_layer_sizes:
+            layer = HiddenLayer(M1, M2)
+            self.layers.append(layer)
+            M1 = M2
 
     # final layer
-    layer = HiddenLayer(M1, 1, lambda x: x)
-    self.layers.append(layer)
+        layer = HiddenLayer(M1, 1, lambda x: x)
+        self.layers.append(layer)
 
     # inputs and targets
-    self.X = tf.placeholder(tf.float32, shape=(None, D), name='X')
-    self.Y = tf.placeholder(tf.float32, shape=(None,), name='Y')
+        self.X = tf.placeholder(tf.float32, shape=(None, D), name='X')
+        self.Y = tf.placeholder(tf.float32, shape=(None,), name='Y')
 
     # calculate output and cost
-    Z = self.X
-    for layer in self.layers:
-      Z = layer.forward(Z)
-    Y_hat = tf.reshape(Z, [-1]) # the output
+        Z = self.X
+        for layer in self.layers:
+            Z = layer.forward(Z)
+        Y_hat = tf.reshape(Z, [-1]) # the output
     
-    self.predict_op = Y_hat
+        self.predict_op = Y_hat
 
-    cost = tf.reduce_sum(tf.square(self.Y - Y_hat))
-    self.cost = cost
-    self.train_op = tf.train.AdamOptimizer(1e-1).minimize(cost)
+        cost = tf.reduce_sum(tf.square(self.Y - Y_hat))
+        self.cost = cost
+        self.train_op = tf.train.AdamOptimizer(1e-1).minimize(cost)
 
-  def set_session(self, session):
-    self.session = session
+    def set_session(self, session):
+        self.session = session
 
-  def partial_fit(self, X, Y):
-    X = np.atleast_2d(X)
-    X = self.ft.transform(X)
-    Y = np.atleast_1d(Y)
+    def partial_fit(self, X, Y):
+        X = np.atleast_2d(X)
+        X = self.ft.transform(X)
+        Y = np.atleast_1d(Y)
     #fit one step
-    self.session.run(self.train_op, feed_dict={self.X: X, self.Y: Y})
+        self.session.run(self.train_op, feed_dict={self.X: X, self.Y: Y})
     
-    cost = self.session.run(self.cost, feed_dict={self.X: X, self.Y: Y})
-    self.costs.append(cost)
+        cost = self.session.run(self.cost, feed_dict={self.X: X, self.Y: Y})
+        self.costs.append(cost)
 
-  def predict(self, X):
-    X = np.atleast_2d(X)
-    X = self.ft.transform(X)
-    return self.session.run(self.predict_op, feed_dict={self.X: X})
+    def predict(self, X):
+        X = np.atleast_2d(X)
+        X = self.ft.transform(X)
+        return self.session.run(self.predict_op, feed_dict={self.X: X})
 
 
     
@@ -232,32 +230,38 @@ def step(observation, action, timestep=1, K=1):
 
 def isDone(reward):
     done = 0
-    collision_info = client.getCollisionInfo()
-    
     if  reward <= -25:
         done = 1
-        
-    elif collision_info.has_collided:
-        done =1 
-        
     return done    
+
+def isDonebyCollide(reward):
+    done_col = 0
+    collision_info = client.getCollisionInfo()
+    if collision_info.has_collided:
+        done_col =1 
+    return done_col  
+
     
     
 def play_one_td(pmodel, vmodel, gamma):
     observation = client.getPosition().z_val - z_wire
-    done = False
+    done = 0
+    done_col=0
     totalreward = 0
 
     while not done:
         action = pmodel.sample_action(observation)
         prev_observation = observation
-    
+        #1 step action
         observation, reward = step(observation,action)
+        
         done = isDone(totalreward)
-
     
         if done:
             reward = -100
+            
+        if done_col:
+            reward = -1000
             
         #if reach terminal state
         if (observation.x_val<=goal_pos[0]+1 and observation.x_val>=goal_pos[0]-1) and  (observation.y_val<=goal_pos[1]+1 and observation.y_val>=goal_pos[1]-1):
@@ -304,46 +308,26 @@ time.sleep(np.abs(to_angle/yaw_rate))
 
 
 ft = FeatureTransformer()
-
-
-
-
-
-
-
-
-def main():
-  env = gym.make('MountainCarContinuous-v0')
-  ft = FeatureTransformer(env, n_components=100)
-  D = ft.dimensions
-  pmodel = PolicyModel(D, ft, [])
-  vmodel = ValueModel(D, ft, [])
-  init = tf.global_variables_initializer()
-  session = tf.InteractiveSession()
-  session.run(init)
-  pmodel.set_session(session)
-  vmodel.set_session(session)
-  gamma = 0.95
-
-
-  N = 50
-  totalrewards = np.empty(N)
-  costs = np.empty(N)
-  for n in range(N):
-    totalreward, num_steps = play_one_td(env, pmodel, vmodel, gamma)
+D = ft.dimensions
+pmodel = PolicyModel(D, ft, [1024,1024])
+vmodel = ValueModel(D, ft, [1024,1024])
+init = tf.global_variables_initializer()
+session = tf.InteractiveSession()
+session.run(init)
+pmodel.set_session(session)
+vmodel.set_session(session)
+gamma = 0.9
+N = 50
+totalrewards = np.empty(N)
+costs = np.empty(N)
+#play N times
+for n in range(N):
+    totalreward, num_steps = play_one_td(pmodel, vmodel, gamma)
     totalrewards[n] = totalreward
-    if n % 1 == 0:
-      print("episode:", n, "total reward: %.1f" % totalreward, "num steps: %d" % num_steps, "avg reward (last 100): %.1f" % totalrewards[max(0, n-100):(n+1)].mean())
+    print("episode:", n, "total reward: %.1f" % totalreward)
+    
+print("avg reward for last 100 episodes:", totalrewards[-100:].mean())
 
-  print("avg reward for last 100 episodes:", totalrewards[-100:].mean())
-
-  plt.plot(totalrewards)
-  plt.title("Rewards")
-  plt.show()
-
-  plot_running_avg(totalrewards)
-  plot_cost_to_go(env, vmodel)
-
-
-if __name__ == '__main__':
-  main()
+plt.plot(totalrewards)
+plt.title("Rewards")
+plt.show()
