781918fb9e92a7626b6f50c7dc206787d32b710e
panin <pienrojp@gmail.com>
2018-04-18 12:35:47 +0200

    newest ver

--- height_control.py
+++ height_control.py
@@ -4,19 +4,13 @@ Created on Thu Apr 12 16:24:22 2018
 
 @author: pienrojp
 """
-
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 11 17:39:32 2018
-
-@author: pienrojp
-"""
 from __future__ import print_function, division
 from builtins import range
 import numpy as np
 from sklearn.pipeline import FeatureUnion
 from sklearn.preprocessing import StandardScaler
 from sklearn.kernel_approximation import RBFSampler
+import matplotlib.pylab as plt
 
 import sys
 sys.path.insert(0, '/home/pienrojp/AirSim/PythonClient')
@@ -26,7 +20,18 @@ from AirSimClient import *
 class SGDRegressor:
   def __init__(self, D):
     self.w = np.random.randn(D) / np.sqrt(D)
-    self.lr = 0.1
+    self.lr = 0.02
+
+  def partial_fit(self, X, Y):
+    self.w += self.lr*(Y - X.dot(self.w)).dot(X)
+
+  def predict(self, X):
+    return X.dot(self.w)
+
+class SGDRegressor_cont:
+  def __init__(self, w):
+    self.w = w
+    self.lr = 0.02
 
   def partial_fit(self, X, Y):
     self.w += self.lr*(Y - X.dot(self.w)).dot(X)
@@ -46,7 +51,7 @@ goal_pos = [12,101,-18]
 #distance to goal
 dist_to_goal = np.sqrt((start_pos[0]-goal_pos[0])**2 + (start_pos[1]-goal_pos[1])**2 )
 #moving velocity
-vel = 3
+vel = 2
 #slope
 m = (goal_pos[1]-start_pos[1])/(goal_pos[0]-start_pos[0])
 #vx and vy
@@ -55,13 +60,13 @@ v_x = vel/m
 
 #desired_height
 z_wire = -15
-z_dist_wanted = 2
+z_dist_wanted = -2
 
 ''' state = (x,y,z) and V(x,y,z) ~ f(x,y,z) where f() is rbf net'''
 class FeatureTransformer:
-  def __init__(self, n_components=500):
+  def __init__(self, n_components=100):
       #lets say [8,-8]
-    observation_examples = 8*(np.random.random((20000, 1))*2 - 1)
+    observation_examples = 11*(np.random.random((20000, 1)) - 0.5)
     scaler = StandardScaler()
     scaler.fit(observation_examples)
 
@@ -86,31 +91,30 @@ class FeatureTransformer:
 
 
 ''' Action '''
-possible_action = [-1, -0.7, -0.5, -0.3, 0, 0.3, 0.5, 0.7, 1]
+possible_action = [0, 1, 2, 3, 4, 5, 6,7,8]
 
 #5 discrete actions
 def interpret_action(action):
     quad_offset = (0, 0, 0)
     
-    if action == -1:
-        quad_offset = (0, 0, -1)
-    elif action == -0.7:
-        quad_offset = (0, 0, -0.7)
-    elif action == -0.5:
-        quad_offset = (0, 0, -0.5)
-    elif action == -0.3:
-        quad_offset = (0, 0, -0.3)
-    elif action == 0:
-        quad_offset = (0, 0, 0)
-    elif action == 0.3:
-        quad_offset = (0, 0, 0.3)
-    elif action == 0.5:
-        quad_offset = (0, 0, 0.5)
-    elif action == 0.7:
-        quad_offset = (0, 0, 0.7)
+    if action == 0:
+        quad_offset = (0, 0, -2)
     elif action == 1:
+        quad_offset = (0, 0, -1)
+    elif action == 2:
+        quad_offset = (0, 0, -0.5)
+    elif action == 3:
+        quad_offset = (0, 0, 0)
+    elif action == 4:
+        quad_offset = (0, 0, 0.5)
+    elif action == 5:
         quad_offset = (0, 0, 1)
-        
+    elif action == 6:
+        quad_offset = (0, 0, 2)
+    elif action == 7:
+        quad_offset = (0, 0, -0.25)
+    elif action == 8:
+        quad_offset = (0, 0, 0.25)        
         
     return quad_offset[2]
     
@@ -133,7 +137,7 @@ class Agent:
     return result
 
   def update(self, observation, a, G):
-    X = self.feature_transformer.transform(np.atleast_2d(s))
+    X = self.feature_transformer.transform(np.atleast_2d(observation))
     self.models[a].partial_fit(X, [G])
 
   def act(self, observation, eps):
@@ -143,13 +147,37 @@ class Agent:
       p = self.predict(observation)
       return np.argmax(p)
       
+class Agent_cont:
+  def __init__(self, feature_transformer,w_models_final):
+    self.feature_transformer =  feature_transformer
+    self.models = []
+    
+    #array of model same number as action
+    for i in range(len(possible_action)):
+        model = SGDRegressor_cont(w_models_final[i])
+        self.models.append(model)
 
+  def predict(self, s):
+    X = self.feature_transformer.transform(np.atleast_2d(s))
+    result = np.stack([m.predict(X) for m in self.models]).T
+    return result
+
+  def update(self, observation, a, G):
+    X = self.feature_transformer.transform(np.atleast_2d(observation))
+    self.models[a].partial_fit(X, [G])
+
+  def act(self, observation, eps):
+    if np.random.random() < eps:
+      return np.random.choice(possible_action)
+    else:   
+      p = self.predict(observation)
+      return np.argmax(p)
     
       
 def step(observation, action, K=1):
     #next observation
-    client.moveByVelocity(v_x, v_y, interpret_action(action) ,time_step)
-    time.sleep(0.5)
+    client.moveByVelocity(v_x, v_y, interpret_action(action) ,1)
+    time.sleep(1)
     
     #like depth from depth camera 
     new_observation = client.getPosition().z_val - z_wire
@@ -160,19 +188,23 @@ def step(observation, action, K=1):
     
 def isDone(reward):
     done = 0
-    collision_info = client.getCollisionInfo()
     
-    if  reward <= -25:
+    if  reward <= -30:
         done = 1
         
-    elif collision_info.has_collided:
-        done =1 
-        
     return done
+    
+def done_by_collide():
+    collision_info = client.getCollisionInfo()
+    done_col = 0
+    if collision_info.has_collided:
+        done_col=1
+    return done_col
     
 def play_one(agent, eps, gamma,start_pos,vel):
     observation = client.getPosition().z_val - z_wire
     done = 0
+    done_col = 0    
     totalreward = 0
     
     while not done:
@@ -188,24 +220,102 @@ def play_one(agent, eps, gamma,start_pos,vel):
         if done:
             reward = -100
             
+        if done_col:
+            reward = -1000
+            G = reward + gamma*np.max(agent.predict(observation))
+            agent.update(prev_observation, action, G)
+            client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],2*vel)
+            return totalreward
+            
+            
         #if reach terminal state
-        if (observation.x_val<=goal_pos[0]+1 and observation.x_val>=goal_pos[0]-1) and  (observation.y_val<=goal_pos[1]+1 and observation.y_val>=goal_pos[1]-1):
+        if (client.getPosition().x_val<=goal_pos[0]+1 and client.getPosition().x_val>=goal_pos[0]-1) and  (client.getPosition().y_val<=goal_pos[1]+1 and client.getPosition().y_val>=goal_pos[1]-1):
             client.hover()
             time.sleep(1)
-            break
+            G = reward + gamma*np.max(agent.predict(observation))
+            agent.update(prev_observation, action, G)
+            client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],2*vel)
+            return totalreward  
 
     # update the model
-        #[0] ??
         G = reward + gamma*np.max(agent.predict(observation))
         agent.update(prev_observation, action, G)
 
         
     #move back to init
-    client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],vel)
+    client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],2*vel)
+    client.hover()
+    time.sleep(1.5)
 
     return totalreward      
     
     
+models_final= np.load('/home/pienrojp/ms_thesis/result/height_Q_contS_disA/model.npy')
+# connect to the AirSim simulator
+client = MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+client.takeoff(1)
+
+
+#go to start pos
+client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],vel)
+#wait
+client.hover()
+time.sleep(1)
+
+
+#Adjust yaw
+yaw_rate = 1
+to_angle = 180*np.arctan((goal_pos[0]-start_pos[0])/(goal_pos[1]-start_pos[1]))/np.pi
+client.rotateByYawRate(-np.sign((goal_pos[0]-start_pos[0])/(goal_pos[1]-start_pos[1]))*yaw_rate, duration=np.abs(to_angle/yaw_rate))
+time.sleep(np.abs(to_angle/yaw_rate))
+
+
+ft = FeatureTransformer()
+agent = Agent(ft)
+#agent = Agent_cont(ft, models_final)
+gamma = 0.9
+
+#training
+N = 60
+totalrewards = np.empty(N)
+for n in range(N):
+    eps = 1/(n+1)
+    totalreward = play_one(agent, eps, gamma,start_pos,vel)
+    totalrewards[n] = totalreward
+    print("episode:", n, "total reward:", totalreward, "eps:", eps)
+
+client.armDisarm(False)
+client.reset()
+
+# that's enough fun for now. let's quit cleanly
+client.enableApiControl(False)
+
+models_final = agent.models
+w_models_final = []
+for i in range(len(agent.models)):
+    w_models_final.append(agent.models[i].w)
+
+
+plt.plot(totalrewards)
+plt.title("Rewards")
+plt.xlabel('episodes')
+plt.ylabel('rewards')
+plt.show()
+
+
+np.save('./result/height_Q_contS_disA/totalrewards', totalrewards)
+np.save('./result/height_Q_contS_disA/model', models_final)
+np.save('./result/height_Q_contS_disA/w_model', w_models_final)
+
+
+
+#replay
+models_final= np.load('/home/pienrojp/ms_thesis/result/height_Q_contS_disA/model.npy')
+w_models_final = np.load('/home/pienrojp/ms_thesis/result/height_Q_contS_disA/w_model.npy')
+
 
 # connect to the AirSim simulator
 client = MultirotorClient()
@@ -221,31 +331,31 @@ client.moveToPosition(start_pos[0],start_pos[1],start_pos[2],vel)
 client.hover()
 time.sleep(1)
 
-'''
+
 #Adjust yaw
 yaw_rate = 1
 to_angle = 180*np.arctan((goal_pos[0]-start_pos[0])/(goal_pos[1]-start_pos[1]))/np.pi
 client.rotateByYawRate(-np.sign((goal_pos[0]-start_pos[0])/(goal_pos[1]-start_pos[1]))*yaw_rate, duration=np.abs(to_angle/yaw_rate))
 time.sleep(np.abs(to_angle/yaw_rate))
-'''
+
 
 ft = FeatureTransformer()
-agent = Agent(ft)
+agent = Agent_cont(ft, w_models_final)
+
+
 gamma = 0.9
 
 #training
-N = 600
+N = 1
 totalrewards = np.empty(N)
 for n in range(N):
-    eps = 1/np.sqrt(n+1)
+    eps = 0
     totalreward = play_one(agent, eps, gamma,start_pos,vel)
     totalrewards[n] = totalreward
-    if n % 5 == 0:
-        print("episode:", n, "total reward:", totalreward, "eps:", eps)
-        print("avg reward for last 5 episodes:", totalrewards[-5:].mean())
+    print("episode:", n, "total reward:", totalreward, "eps:", eps)
 
-Q_final = agent.Q        
+client.armDisarm(False)
+client.reset()
 
-plt.plot(totalrewards)
-plt.title("Rewards")
-plt.show()
+# that's enough fun for now. let's quit cleanly
+client.enableApiControl(False)
